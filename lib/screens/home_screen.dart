b9a1301059cda7e9aaaf15316ef2a75e370632c3
jm_b <jmbanchero@gmail.com>
2022-07-29 21:53:08 +0100

    add list

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -72,8 +72,45 @@ class CustomScreen extends StatelessWidget {
     return Container(
       color: colorUno,
       child: Center(
-        child: Text(texto.asCamelCase),
+        // child: Text(texto.asCamelCase),
+        child: RandomWords(),
       ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: /*1*/ (context, i) {
+        if (i.isOdd) return const Divider(); /*2*/
+
+        final index = i ~/ 2; /*3*/
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+        }
+        return ListTile(
+          title: Text(_suggestions[index].asCamelCase),
+          textColor: Colors.white,
+          trailing: Icon(
+            Icons.favorite_border,
+            color: Colors.redAccent,
+          ),
+          // trailing: Icon(color: Colors.red),
+        );
+      },
     );
   }
 }
